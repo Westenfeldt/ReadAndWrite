c51f9f50a624c392d89343637fea50f76055285b
Stefan Vestenfeldt <stefanwestenfeldt@gmail.com>
2019-08-13 12:37:42 +0200

    Add project files.

---
+++ ReadAndWrite/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ReadAndWrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname;
+            string lastname;
+            string age;
+            //double age;
+
+            Console.WriteLine("Hvad er dit fornavn?");
+
+            firstname = Console.ReadLine();
+
+            Console.WriteLine("Hvad er dit efternavn?");
+
+            lastname = Console.ReadLine();
+
+            Console.WriteLine("Hvad er din alder?");
+
+            age = Console.ReadLine();
+
+        
+
+
+
+
+
+
+
+            Console.WriteLine("Du hedder {0} {1} og er {2} år gammel.", firstname, lastname, age);
+        }
+    }
+}
